bf2728adffdb259b63c3d772725173525cd28cd6
Gracya Sidabutar <130347439+graacyyy@users.noreply.github.com>
2024-12-21 02:10:07 +0700

    Update navbar.tsx

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -4,10 +4,15 @@ import { useState, useEffect } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
+import { authClient } from "@/lib/auth-client";
+import { redirect, useRouter } from "next/navigation";
 
 export function Navbar() {
   const [isScrolled, setIsScrolled] = useState(false);
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const { useSession, signOut } = authClient;
+  const { data: session } = useSession();
+  const router = useRouter();
 
   useEffect(() => {
     const handleScroll = () => {
@@ -29,11 +34,18 @@ export function Navbar() {
     setIsMenuOpen(!isMenuOpen);
   };
 
+  const handleLogout = async () => {
+    await signOut();
+    router.push("/login");
+  };
+
   return (
     <header
-      className={`fixed left-0 right-0 top-0 z-50 ${isScrolled ? "bg-white text-black" : "bg-transparent text-white"} transition-all duration-300`}
+      className={`fixed left-0 right-0 top-0 z-50 flex justify-center ${
+        isScrolled ? "bg-white text-black" : "bg-transparent text-white"
+      } transition-all duration-300`}
     >
-      <div className="container flex h-16 items-center justify-between px-4">
+      <div className="container flex h-16 items-center justify-between px-4 w-screen">
         <Link href="/" className="flex text-xl font-semibold">
           <Image
             src={isScrolled ? "/logo-dark.png" : "/logo.png"}
@@ -43,6 +55,19 @@ export function Navbar() {
           />
           <h1 className="ml-3 mr-10">Styla</h1>
         </Link>
+
+        {session && (
+          <Button
+            onClick={handleLogout}
+            className={`${
+              isScrolled
+                ? "bg-pink-500 text-white hover:bg-pink-600"
+                : "bg-white text-pink-500 hover:bg-gray-100"
+            }`}
+          >
+            Logout
+          </Button>
+        )}
       </div>
     </header>
   );
